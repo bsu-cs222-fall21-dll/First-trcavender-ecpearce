e42f3f692ca2d30c6da80126777f8d7a18038739
trcavender <trcavender@bsu.edu>
2021-09-16 16:30:35 -0400

    Fixed missing () in Main Class

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        UserInterface userInterface = new UserInterface;
+        UserInterface userInterface = new UserInterface();
         userInterface.displayInterface();
     }
 }
