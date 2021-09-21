e5452c748e7b5464f23f771d969540e530330419
trcavender <trcavender@bsu.edu>
2021-09-21 16:26:31 -0400

    Added method UserSearchWikipedia to the class ReceiveUserInput.java.

--- src/main/java/ReceiveUserInput.java
+++ src/main/java/ReceiveUserInput.java
@@ -1,2 +1,9 @@
+import javax.swing.JOptionPane;
+
 public class ReceiveUserInput {
+
+    public String UserSearchWikipedia(){
+        String userInput = JOptionPane.showInputDialog("What would you like to search?");
+        return userInput;
+    }
 }
