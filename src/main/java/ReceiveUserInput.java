fb148b6cd8ad38eb5965cf7262bb044cf1e9f245
trcavender <trcavender@bsu.edu>
2021-09-21 19:51:19 -0400

    Updated the method userSearchWikipedia in the ReceiveUserInput.java class by making it static.

--- src/main/java/ReceiveUserInput.java
+++ src/main/java/ReceiveUserInput.java
@@ -2,7 +2,7 @@ import javax.swing.JOptionPane;
 
 public class ReceiveUserInput {
 
-    public String userSearchWikipedia(){
+    public static String userSearchWikipedia() {
         String userInput = JOptionPane.showInputDialog("What would you like to search?");
         return userInput;
     }
