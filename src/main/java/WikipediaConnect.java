5810904568e4669aa5d56bcf76dce38962eb96c0
ecpearce <ecpearce@bsu.edu>
2021-09-23 15:40:28 -0400

    added open Connection, which will get the original website in order to use it in getWIkiPage (i think)

--- src/main/java/WikipediaConnect.java
+++ src/main/java/WikipediaConnect.java
@@ -1,25 +1,26 @@
 import java.lang.Object;
 import java.net.URLEncoder;
 import java.net.URLConnection;
+import java.io.InputStream;
+
 
 public class WikipediaConnect {
-
-    public URLConnection openConnection(){
-        //https://en.wikipedia.org/wiki/Special:ApiSandbox
-        // use sandbox to find recent updates?
-
-        URL url = new URL("https://en.wikipedia.org");
-        URLConnection connection = url.openConnection();
+    URL url = new URL("https://en.wikipedia.org");
+    URLConnection connection = url.openConnection();
         connection.setRequestProperty("User-Agent", "Revision Reporter/0.1 (me@bsu.edu)");
-        InputStream inputStream = connection.getInputStream();
+    InputStream inputStream = connection.getInputStream();
 
-
+    public URLConnection openConnection() {
+        return null;
     }
 
     public String getWikiPage(String UserInput){
         static String encode(UserInput, "www.wikipedia.com");
 
     }
+
+
+
 
 
 
