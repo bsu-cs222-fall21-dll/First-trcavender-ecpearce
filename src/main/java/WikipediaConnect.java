b08c9caa3b71f4328bfbfa375ec83e81d76204a9
ecpearce <ecpearce@bsu.edu>
2021-09-23 15:48:19 -0400

    still trying to connect to wiki page...

--- src/main/java/WikipediaConnect.java
+++ src/main/java/WikipediaConnect.java
@@ -1,14 +1,18 @@
+import java.io.IOException;
 import java.lang.Object;
-import java.net.URLEncoder;
 import java.net.URLConnection;
 import java.io.InputStream;
 
 
 public class WikipediaConnect {
-    URL url = new URL("https://en.wikipedia.org");
-    URLConnection connection = url.openConnection();
-    connection.setRequestProperty("User-Agent", "Revision Reporter/0.1 (me@bsu.edu)");
-    InputStream inputStream = connection.getInputStream();
+    private static URLConnection getWikiPage() throws IOException{
+        URL url = new URL("https://en.wikipedia.org");
+        URLConnection connection = url.openConnection();
+        connection.setRequestProperty("User-Agent", "Revision Reporter/0.1 (me@bsu.edu)");
+        InputStream inputStream = connection.getInputStream();
+    }
+
+
 
     public URLConnection openConnection() {
         return null;
