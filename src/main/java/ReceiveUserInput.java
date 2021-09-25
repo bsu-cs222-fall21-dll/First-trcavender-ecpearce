fea7f5b2a3c6c4eb6fe5c541866744ae6361b38e
trcavender <trcavender@bsu.edu>
2021-09-25 10:50:53 -0400

    Added new method completeUrl to the ReceiveUserInput.java class. The idea for this method came from the work of wjgibson and wtwithers.

--- src/main/java/ReceiveUserInput.java
+++ src/main/java/ReceiveUserInput.java
@@ -6,4 +6,15 @@ public class ReceiveUserInput {
         String userInput = JOptionPane.showInputDialog("What would you like to search?");
         return userInput;
     }
+
+    public String completeUrl(String userInput) {
+        String beginningUrl = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=";
+        String userUrl = userInput;
+        String endUrl = "&redirects=1&callback=&rvprop=timestamp%7Cuser&rvlimit=30";
+        String completeUrl = beginningUrl + userUrl + endUrl;
+
+        return completeUrl;
+
+        //The idea of this method came from the work of wjgibson and wtwithers.
+    }
 }
