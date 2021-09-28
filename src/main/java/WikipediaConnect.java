decc83a93b1de5eeee097b50238cdb2b0216558a
trcavender <trcavender@bsu.edu>
2021-09-27 20:53:28 -0400

    Updated WikipediaConnect.java with a new structure.

--- src/main/java/WikipediaConnect.java
+++ src/main/java/WikipediaConnect.java
@@ -1,19 +1,26 @@
 import java.io.IOException;
 import java.lang.Object;
+import java.net.ConnectException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.io.InputStream;
 
 
 public class WikipediaConnect {
-        private static URLConnection makeConnection(String Url) throws IOException{
-            ReceiveUserInput receiveUserInput = new ReceiveUserInput();
-             URL url = new URL(receiveUserInput.completeUrl(ReceiveUserInput.userSearchWikipedia()));
-             URLConnection connection = url.openConnection();
-             connection.setRequestProperty("User-Agent", "Revision Reporter/0.1 (ecpearce@bsu.edu)");
-             InputStream inputStream = connection.getInputStream();
-             connection.connect();
-             return connection;
+        public InputStream URLConnection (String Url) throws IOException{
+            try {
+                URL url = new URL(Url);
+                URLConnection connection = url.openConnection();
+                connection.setRequestProperty("User-Agent", "FirstProject/0.1 (ecpearce@bsu.edu)");
+                InputStream inputStream = connection.getInputStream();
+                connection.connect();
+                return inputStream;
+            }
+            catch(ConnectException connectException){
+                System.err.println("Unable to establish connection");
+                System.exit(3);
+                return null;
+            }
         }
 
 
